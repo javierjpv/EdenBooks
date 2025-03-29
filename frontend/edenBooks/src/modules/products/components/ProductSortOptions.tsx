483cdddce5fafccd121bbb28d1e85a6931875cf1
javier <paredesvelezjavierr@gmail.com>
2025-03-29 18:23:59 +0100

    fixing order button responsive

--- frontend/edenBooks/src/modules/products/components/ProductSortOptions.tsx
+++ frontend/edenBooks/src/modules/products/components/ProductSortOptions.tsx
@@ -1,31 +1,86 @@
-import { Button, Box } from "@mui/material";
+import {
+  Button,
+  Box,
+  Popover,
+  List,
+  ListItem,
+  ListItemButton,
+} from "@mui/material";
 import { useSearchParams } from "react-router";
-
-export const ProductSortOptions: React.FC = () => {
+import { useState } from "react";
+interface ProductSortOptionsProps {
+  isMediumScreen: boolean;
+}
+export const ProductSortOptions = ({
+  isMediumScreen,
+}: ProductSortOptionsProps) => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
-  const handleSortChange = (order: "asc" | "desc",sortBy: "price") => {
+  const handleSortChange = (order: "asc" | "desc", sortBy: "price") => {
     searchParams.set("order", order);
     searchParams.set("sort_by", sortBy);
     setSearchParams(searchParams);
+    setAnchorEl(null); // Cierra el menú después de seleccionar
   };
 
   return (
-    <Box display="flex" gap={2} sx={{ mb: 2 }}>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => handleSortChange("asc","price")}
-      >
-        Precio Ascendente
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={() => handleSortChange("desc","price")}
-      >
-        Precio Descendente
-      </Button>
+    <Box display="flex" gap={2} >
+      {!isMediumScreen && (
+        <>
+          <Button
+            variant="contained"
+            color="info"
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+          >
+            Ordenar
+          </Button>
+          <Popover
+            open={open}
+            anchorEl={anchorEl}
+            onClose={() => setAnchorEl(null)}
+            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+            transformOrigin={{ vertical: "top", horizontal: "left" }}
+          >
+            <List>
+              <Button>Ordenar</Button>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => handleSortChange("asc", "price")}
+                >
+                  Precio Ascendente
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => handleSortChange("desc", "price")}
+                >
+                  Precio Descendente
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Popover>
+        </>
+      )}
+      {isMediumScreen && (
+        <>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handleSortChange("asc", "price")}
+          >
+            Precio Ascendente
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => handleSortChange("desc", "price")}
+          >
+            Precio Descendente
+          </Button>
+        </>
+      )}
     </Box>
   );
 };
--- frontend/edenBooks/src/modules/products/pages/MyProductPage.tsx
+++ frontend/edenBooks/src/modules/products/pages/MyProductPage.tsx
@@ -1,4 +1,13 @@
-import { Button, Container, Drawer, Grid2, Typography, useMediaQuery, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Drawer,
+  Grid2,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { ProductList } from "../components/ProductList";
 import { ProductFilters } from "../components/ProductFilters";
 import { useState } from "react";
@@ -7,29 +16,34 @@ import { ProductSortOptions } from "../components/ProductSortOptions";
 export const MyProductPage = () => {
   const [openDrawer, setOpenDrawer] = useState<boolean>(false);
   const theme = useTheme();
-  const isMediumScreen = useMediaQuery(theme.breakpoints.up("md"));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.up("md")); //El breakpoint de md lo veo mas util
   const isMobileScreen = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <>
-      <Container sx={{ marginTop:22 }}>
-        <Typography textAlign={"center"} variant="h4" >My ProductPage</Typography>
-
-        {/* Botón para abrir los filtros solo en móviles */}
-        {!isMediumScreen && (
-          <Button
-            variant="contained"
-            color="info"
-            onClick={() => setOpenDrawer(true)}
-            sx={{ ml: 2 }}
-          >
-            Filtros
-          </Button>
-        )}
+      <Container sx={{ marginTop: 22 }}>
+        <Typography textAlign={"center"} variant="h4">
+          My ProductPage
+        </Typography>
+        <Box display={"flex"} gap={1.5}>
+          {/* Botón para abrir los filtros solo en móviles */}
+          {!isMediumScreen && (
+            <Button
+              variant="contained"
+              color="info"
+              onClick={() => setOpenDrawer(true)}
+            >
+              Filtrar
+            </Button>
+          )}
+          {!isMediumScreen && (
+            <ProductSortOptions isMediumScreen={isMediumScreen} />
+          )}
+        </Box>
 
         <Grid2 container spacing={2}>
           {/* Filtros como columna en pantallas medianas y grandes */}
           {isMediumScreen ? (
-            <Grid2 size={{ xs:12,md: 3 }}>
+            <Grid2 size={{ xs: 12, md: 3 }}>
               <ProductFilters setOpenDrawer={setOpenDrawer} />
             </Grid2>
           ) : (
@@ -48,9 +62,10 @@ export const MyProductPage = () => {
             </Drawer>
           )}
 
-         
-          <Grid2 size={{xs:12,md: 9 }}>
-          <ProductSortOptions />
+          <Grid2 size={{ xs: 12, md: 9 }}>
+            {isMediumScreen && (
+              <ProductSortOptions isMediumScreen={isMediumScreen} />
+            )}
             <ProductList />
           </Grid2>
         </Grid2>
